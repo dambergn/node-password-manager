4695111b9ed803f40ffc430c51cf3426af51b0b9
dambergn <dambergn@gmail.com>
2019-12-19 13:48:09 -0800

    added pbkdf2.js

---
+++ public/assets/js/pbkdf2.js
@@ -0,0 +1,40 @@
+'use strict';
+// password = 'password to be hashed'
+// salt = 
+// iterations = 100000 
+// length = 128
+// hashType = 'sha512'
+
+function pbkdf2(password, salt, iterations, len, hashType) {
+  hashType = hashType || 'sha1';
+  if (!Buffer.isBuffer(password)) {
+    password = new Buffer(password);
+  }
+  if (!Buffer.isBuffer(salt)) {
+    salt = new Buffer(salt);
+  }
+  var out = new Buffer('');
+  var md, prev, i, j;
+  var num = 0;
+  var block = Buffer.concat([salt, new Buffer(4)]);
+  while (out.length < len) {
+    num++;
+    block.writeUInt32BE(num, salt.length);
+    prev = crypto.createHmac(hashType, password)
+      .update(block)
+      .digest();
+    md = prev;
+    i = 0;
+    while (++i < iterations) {
+      prev = crypto.createHmac(hashType, password)
+        .update(prev)
+        .digest();
+      j = -1;
+      while (++j < prev.length) {
+        md[j] ^= prev[j]
+      }
+    }
+    out = Buffer.concat([out, md]);
+  }
+  return out.slice(0, len);
+}
