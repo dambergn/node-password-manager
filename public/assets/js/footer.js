77cb7a521bb4c0af2399b36d0e0494dfa15c3fbe
dambergn <dambergn@gmail.com>
2023-08-22 13:02:06 -0500

    fixed more db issues

--- public/assets/js/footer.js
+++ public/assets/js/footer.js
@@ -1,4 +1,4 @@
 'use strict'
 
-document.write(`<h6>Site Version: Beta 0.2</h6>`);
-document.write(`<h6>Last Updated: 2020-02-13</h6>`);
+document.write(`<h6>Site Version: Beta 0.3</h6>`);
+document.write(`<h6>Last Updated: 2022-08-22</h6>`);
